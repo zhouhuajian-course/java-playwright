190dda90008c3180224132be092cdeabce2c2756
zhouhuajian <zhouhuajian@company.com>
2024-09-12 11:11:59 +0800

    03 第一个 Playwright 脚本

---
+++ learn/src/main/java/learn/FirstScript.java
@@ -0,0 +1,20 @@
+package learn;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class FirstScript {
+  public static void main(String[] args) {
+    try(Playwright playwright = Playwright.create()) {
+      Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+      Page page = browser.newPage();
+      page.navigate("http://localhost/");
+      String title = page.title();
+      String content = page.content();
+      System.out.println(title);
+      System.out.println(content);
+    }
+  }
+}

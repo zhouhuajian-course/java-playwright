db229e199c97c71a1943cec00b4ec1b13874bd52
zhouhuajian <zhouhuajian@company.com>
2024-09-27 10:55:42 +0800

    07 元素操作 表单填写 文件上传

--- learn/src/main/java/learn/FirstScript.java
+++ learn/src/main/java/learn/FirstScript.java
@@ -7,7 +7,7 @@ import com.microsoft.playwright.Playwright;
 
 public class FirstScript {
   public static void main(String[] args) {
-    try(Playwright playwright = Playwright.create()) {
+    try (Playwright playwright = Playwright.create()) {
       Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
       Page page = browser.newPage();
       page.navigate("http://localhost/");
